bb063f098a5cdb3d8cb34823167147ab4acb7b3c
leodorn <leo.dornpro@gmail.com>
2020-12-20 21:18:11 +0100

    inital

--- App.js
+++ App.js
@@ -1,21 +1,96 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {  Text, View,Image,Button, Alert } from 'react-native';
+import firebaseConfig from './config'
+import styles from './styles'
+import * as firebase from 'firebase';
+import * as ImagePicker from 'expo-image-picker';
+import SelectImage from './SelectImage';
+import Ingredient from './Ingredient';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+if(!firebase.apps.length)
+{
+  firebase.initializeApp(firebaseConfig);
+}
+else
+{
+  firebase.app();
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+class App extends React.Component
+{
+
+  state = {
+    ingredients : [],
+    urlImage : null,
+    textChanging : 'Bonjour à tous ! ',
+    imagePick : '',
+    selectedImage :  ''
+  }
+
+
+  
+  
+
+  openImagePickerAsync = async () => {
+    let permissionResult = await ImagePicker.requestCameraPermissionsAsync();
+    permissionResult.granted = true
+    if (permissionResult.granted === false) {
+      console.log("coucou")
+      return;
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+    if (pickerResult.cancelled === true) {
+      return;
+    }
+    this.setState({
+      selectedImage : pickerResult.uri
+    })
+    return;
+  }
+  
+
+  componentDidMount()
+  {
+    firebase.database().ref('ingredients').on('value', snapshot => {
+      this.setState({
+      ingredients : snapshot.val(),
+      loading: false
+      })
+    })
+    /*firebase.storage().ref('Images/fillPot.jpg').getDownloadURL().then((url) =>
+    {
+      this.setState({
+        urlImage: url
+      })
+    });*/
+   
+  }  
+
+
+  /*uploadImage = async (urinimageName) => {
+    const response = await fetch(uri);
+    const blob = await response.blob();
+    var ref = firebase.storage().ref().child("Images/" + imageName);
+    return ref.put(blob);
+  }*/
+
+  render()
+  {
+    const ingredients = this.state.ingredients.map((ingredient) => {
+      return (<Text>{ingredient.name}</Text>)
+    })
+    return (
+      <View style={styles.container}>
+        <Ingredient name = "tomatooooo" image = {require('./tomates.jpg')} />
+      </View>
+    );
+  }
+}
+
+export default App;
+
+
---
+++ config.js
@@ -0,0 +1,19 @@
+import * as firebase from 'firebase';
+
+// Optionally import the services that you want to use
+//import "firebase/auth";
+//import "firebase/database";
+//import "firebase/firestore";
+//import "firebase/functions";
+//import "firebase/storage";
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDy6SE-bov_b3hbBwF2MaSEUdHxXiSOOhA",
+  authDomain: "cookingtest-84d32.firebaseapp.com",
+  projectId: 'cookingtest-84d32',
+  databaseURL: "https://cookingtest-84d32-default-rtdb.firebaseio.com/",
+  storageBucket: "cookingtest-84d32.appspot.com"
+};
+
+export default firebaseConfig;
